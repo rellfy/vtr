826563e74e93338c17b4f862d85bdb896b2916f2
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-18 06:54:10 +1000

    Add example tests

---
+++ src/__tests__/example.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { render } from '@testing-library/react';
+import { Main } from "../components/Main";
+
+test("Main says 'hello world'", () => {
+    const { container } = render(<Main />);
+    expect(container).toHaveTextContent("hello world");
+});
+
+test("Main has a button", () => {
+    const { getByTestId } = render(<Main />);
+    const button = getByTestId("main.button");
+    expect(button).not.toBeNull();
+});
