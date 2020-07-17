43db8465cdd70ce3c5216ee8f3b1e8876449ebd0
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-18 06:52:23 +1000

    Add basic exmaple component

---
+++ src/components/Main.tsx
@@ -0,0 +1,24 @@
+﻿import React from "react";
+import styled from "styled-components";
+import blue from "../../media/blue.png";
+
+export interface Props { }
+export interface State { }
+
+export class Main extends React.Component<Props, State> {
+    
+    public render() {
+        return (
+            <div>
+                hello world<br/>
+                <Button data-testid={"main.button"}>this is a button</Button><br/>
+                this is an image:<br/>
+                <img src={blue}/>
+            </div>
+        );
+    }
+}
+
+const Button = styled.button`
+    color: grey;
+`;
