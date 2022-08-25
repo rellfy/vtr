8f01d307586ba8393650bb9f672d7da7f6140154
Lorenzo <lorenzo@nioel.com>
2022-08-25 22:19:19 +1000

    use router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 ﻿import ReactDOM from "react-dom";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Main from "./components/Main";
 
 ReactDOM.render(
     <Router>
-        <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </Router>,
     document.getElementById("root")
 );
