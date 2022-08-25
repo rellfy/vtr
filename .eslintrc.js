d43eb31ad6169914c0ba44dab312e94a291ddf7d
Lorenzo <lorenzo@nioel.com>
2022-08-25 22:45:58 +1000

    add husky, eslint, and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: [".eslintrc.js", "dist/**/*", "node_modules/**/*"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "no-unused-vars": "error"
+  }
+};
